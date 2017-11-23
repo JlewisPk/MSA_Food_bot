5f4e82174cecc951e9570ddb9ac9ba5e982e210b
JlewisPk <lewis4692@hotmail.com>
2017-11-23 15:54:04 +1300

    Add all

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "e62af88e-5a47-422d-9b7a-b072c8882c44",
+    appPassword: "aykePSAHBG291{[ovlP46{@"
 });
 
 // Listen for messages from users 
